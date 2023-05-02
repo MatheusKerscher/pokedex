7c11d453a23bdcbe936f53c786ed2b450e3cffff
MatheusKerscher <kerschermatheus12@gmail.com>
2023-05-02 13:28:57 -0300

    Alteração nos cards dos pokemons, adicionando mais estatisitcas sobre eles no verso

--- src/app/components/card-pokemon/card-pokemon.component.ts
+++ src/app/components/card-pokemon/card-pokemon.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { Types } from 'src/app/shared/models/pokemon/pokemon.model';
+import { Stats, Types } from 'src/app/shared/models/pokemon/pokemon.model';
 
 @Component({
   selector: 'app-card-pokemon',
@@ -8,13 +8,137 @@ import { Types } from 'src/app/shared/models/pokemon/pokemon.model';
 })
 export class CardPokemonComponent implements OnInit {
   @Input() classForPokemonType?: string;
-  @Input() types?: Types[]
+  @Input() types?: Types[];
   @Input() height?: number;
-  @Input() weight?: number
+  @Input() weight?: number;
+  @Input() pokemonId?: number;
   @Input() imgPokemonUrl?: string;
   @Input() pokemonName?: string;
+  @Input() pokemonStats?: Stats[];
 
   constructor() {}
 
   ngOnInit(): void {}
+
+  setBackgoundGradiente(id: string, statsValue: number) {
+    const element = document.getElementById(id)!;
+
+    let gradientBackground = '';
+
+    switch (this.classForPokemonType) {
+      case 'type-normal':
+        gradientBackground = `linear-gradient(to right, #6a75b8 ${statsValue}%, #8f8d9a  ${
+          statsValue + 0.1
+        }%)`;
+
+        break;
+      case 'type-fire':
+        gradientBackground = `linear-gradient(to right, #1fbdff ${statsValue}%, #c0a099  ${
+          statsValue + 0.1
+        }%)`;
+
+        break;
+      case 'type-water':
+        gradientBackground = `linear-gradient(to right, #d8d87b ${statsValue}%, #e8ecfd  ${
+          statsValue + 0.1
+        }%)`;
+
+        break;
+      case 'type-electric':
+        gradientBackground = `linear-gradient(to right, #24e0ff ${statsValue}%, #d7d57e  ${
+          statsValue + 0.1
+        }%)`;
+
+        break;
+      case 'type-grass':
+        gradientBackground = `linear-gradient(to right, #9c14ff ${statsValue}%, #9c9c8f  ${
+          statsValue + 0.1
+        }%)`;
+
+        break;
+      case 'type-ice':
+        gradientBackground = `linear-gradient(to right, #f17e8a ${statsValue}%, #c0bdbc  ${
+          statsValue + 0.1
+        }%)`;
+
+        break;
+      case 'type-fighting':
+        gradientBackground = `linear-gradient(to right, #00b1ea ${statsValue}%, #a77985  ${
+          statsValue + 0.1
+        }%)`;
+
+        break;
+      case 'type-poison':
+        gradientBackground = `linear-gradient(to right, #00e14f ${statsValue}%, #967f8c  ${
+          statsValue + 0.1
+        }%)`;
+
+        break;
+      case 'type-ground':
+        gradientBackground = `linear-gradient(to right, #48b8ff ${statsValue}%, #c6bc9b  ${
+          statsValue + 0.1
+        }%)`;
+
+        break;
+      case 'type-flying':
+        gradientBackground = `linear-gradient(to right, #c5dba7 ${statsValue}%, #b7a9db  ${
+          statsValue + 0.1
+        }%)`;
+
+        break;
+      case 'type-psychic':
+        gradientBackground = `linear-gradient(to right, #4ffff0 ${statsValue}%, #e09aa8  ${
+          statsValue + 0.1
+        }%)`;
+
+        break;
+      case 'type-bug':
+        gradientBackground = `linear-gradient(to right, #0040d3 ${statsValue}%, #988f6c  ${
+          statsValue + 0.1
+        }%)`;
+
+        break;
+      case 'type-rock':
+        gradientBackground = `linear-gradient(to right, #0076ec ${statsValue}%, #a39179  ${
+          statsValue + 0.1
+        }%)`;
+
+        break;
+      case 'type-ghost':
+        gradientBackground = `linear-gradient(to right, #6fa846 ${statsValue}%, #79737f  ${
+          statsValue + 0.1
+        }%)`;
+
+        break;
+      case 'type-dragon':
+        gradientBackground = `linear-gradient(to right, #32ff32 ${statsValue}%, #8c83c6  ${
+          statsValue + 0.1
+        }%)`;
+
+        break;
+      case 'type-dark':
+        gradientBackground = `linear-gradient(to right, #3f6577 ${statsValue}%, #645c56  ${
+          statsValue + 0.1
+        }%)`;
+
+        break;
+      case 'type-steel':
+        gradientBackground = `linear-gradient(to right, #caccb9 ${statsValue}%, #bebec7  ${
+          statsValue + 0.1
+        }%)`;
+
+        break;
+      case 'type-fairy':
+        gradientBackground = `linear-gradient(to right, #8ecdb8 ${statsValue}%, #c39fb1  ${
+          statsValue + 0.1
+        }%)`;
+
+        break;
+
+      default:
+        break;
+    }
+
+    element.style.background = gradientBackground;
+  }
 }
